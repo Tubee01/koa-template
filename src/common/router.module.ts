fbdaf4836e4062ca56f98ad927e5ede80788fea6
Bence Tamási <tamasi.bence@outlook.com>
2023-03-20 20:48:20 +0100

    refactor: add enum httpmethod, change controller type

--- src/common/router.module.ts
+++ src/common/router.module.ts
@@ -1,8 +1,16 @@
 import Application, { Context, Next } from "koa";
 import Router from "@koa/router";
 
+enum HTTPMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE',
+  PATCH = 'PATCH',
+}
+
 interface ClassConstructor<T> {
-  new (...args: any[]): T;
+  new(...args: any[]): T;
 }
 
 export class RouterModule<T> {
@@ -23,28 +31,28 @@ export class RouterModule<T> {
   }
 
   public init() {
-    this.controllers.map((controller: any, i: number) => {
+    this.controllers.map((controller: ClassConstructor<T>) => {
       const { logger } = this.app.context;
       const { routes } = controller.prototype;
 
-      routes.forEach((route: { path: string, method: string, handler: Function }) => {
+      routes.forEach((route: { path: string, method: HTTPMethod, handler: Function }) => {
         const { path, method, handler } = route;
 
         logger.verbose(`[${this.constructor.name}]: ${method} ${path}`);
         switch (method) {
-          case 'GET':
+          case HTTPMethod.GET:
             this.router.get(path, async (ctx: Context, next: Next) => new controller(this.app)[handler.name](ctx, next));
             break;
-          case 'POST':
+          case HTTPMethod.POST:
             this.router.post(path, async (ctx: Context, next: Next) => new controller(this.app)[handler.name](ctx, next));
             break;
-          case 'PUT':
+          case HTTPMethod.PUT:
             this.router.put(path, async (ctx: Context, next: Next) => new controller(this.app)[handler.name](ctx, next));
             break;
-          case 'DELETE':
+          case HTTPMethod.DELETE:
             this.router.delete(path, async (ctx: Context, next: Next) => new controller(this.app)[handler.name](ctx, next));
             break;
-          case 'PATCH':
+          case HTTPMethod.PATCH:
             this.router.patch(path, async (ctx: Context, next: Next) => new controller(this.app)[handler.name](ctx, next));
             break;
           default:
